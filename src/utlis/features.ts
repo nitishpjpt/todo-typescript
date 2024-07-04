444b64ad6792669437dcd172dfa33231ba55d636
nitishpjpt <nitishpjpt97@gmail.com>
2024-07-04 12:03:12 +0530

    first commit

---
+++ src/utlis/features.ts
@@ -0,0 +1,15 @@
+
+type TodoItemType = {
+    title: string;
+    isCompleted: boolean;
+    id: string;
+  };
+
+export const saveTodos = (todos: TodoItemType[]): void => {
+    localStorage.setItem("mytodos", JSON.stringify(todos));
+  };
+  
+  export const getTodos = (): TodoItemType[] => {
+    const todos = localStorage.getItem("mytodos");
+    return todos ? JSON.parse(todos) : [];
+  };
